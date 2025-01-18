bad8d7a778604950ede7e07f119418f035b3712a
bmxcmx <cadenwhitfield86@gmail.com>
2025-01-18 15:39:10 -0500

    Starting an elevator subsystem

---
+++ src/main/java/frc/robot/subsystems/Elevator/ElMotorIO.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.Elevator;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.subsystems.drive.ModuleIO.ModuleIOInputs;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ElMotorIO {
+
+  @AutoLog
+  public static class ElMotorIOInputs {
+    public boolean masterConnected = false;
+    public double masterPositionRad = 0.0;
+    public double masterVelocityRadPerSec = 0.0;
+    public double masterAppliedVolts = 0.0;
+    public double masterCurrentAmps = 0.0;
+
+    public boolean followerConnected = false;
+    public double followerPositionRad = 0.0;
+    public double followerVelocityRadPerSec = 0.0;
+    public double followerAppliedVolts = 0.0;
+    public double followerCurrentAmps = 0.0;
+
+    public Rotation2d extentionAbsPos = new Rotation2d();
+    public Rotation2d extentionPos = new Rotation2d();
+  }
+    /** Updates the set of loggable inputs. */
+  public default void updateInputs(ModuleIOInputs inputs) {}
+
+  /** Run the drive motor at the specified open loop value. */
+  public default void setElevatorOpenLoop(double output) {}
+
+  /** Run the drive motor at the specified velocity. */
+  public default void setElevatorVelocity(double velocityRadPerSec) {}
+
+  /** Run the turn motor to the specified rotation. */
+  public default void setElevatorPosition(Rotation2d rotation) {}
+}
